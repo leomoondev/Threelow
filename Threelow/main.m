007ff096c6b56411a50c60b1d5cfbb698abf2aa3
Leo Moon <leomoondev@gmail.com>
2017-02-19 23:01:13 -0800

    Initial project files added

---
+++ Threelow/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Threelow
+//
+//  Created by Hyung Jip Moon on 2017-02-19.
+//  Copyright © 2017 leomoon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
